635b825f8d5fb0dff4426d95c61648245debfbff
saeed <msabdi1377@gmail.com>
2026-05-21 20:36:22 +0330

    fix: import

--- atashin_gallery_backend/urls.py
+++ atashin_gallery_backend/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from decoupe import config
+from decouple import config
 
 urlpatterns = [
     path('config("ADMIN_URL")/', admin.site.urls),
