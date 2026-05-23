54a11dd521ae019638167e2769a73127317404fb
saeed <msabdi1377@gmail.com>
2026-05-23 21:35:09 +0330

    restyle: urls

--- atashin_gallery_backend/urls.py
+++ atashin_gallery_backend/urls.py
@@ -15,10 +15,33 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from decouple import config
 
+VERSION = 1
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version=VERSION,
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
-    path('config("ADMIN_URL")/', admin.site.urls),
+    path(f'{config("ADMIN_URL")}/', admin.site.urls),
+    path(f'{config("SWAGGER_URL")}/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(f'api/v{VERSION}/product/', include('product.urls')),
 ]
