7ace0d63cda855e6e8d3c31620f510403f84b1e8
saeed <msabdi1377@gmail.com>
2026-05-26 00:21:12 +0330

    feat: user admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import User, Province, City, Address
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['phone_number', 'first_name', 'last_name', 'is_staff']
+    list_filter = ['is_staff', 'is_active']
+    search_fields = ['phone_number', 'first_name', 'last_name']
+    ordering = ['-date_joined']
+
+@admin.register(Province)
+class ProvinceAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['name', 'province']
+    list_filter = ['province']
+    search_fields = ['name']
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['user', 'city', 'postal_code']
+    search_fields = ['user__phone_number', 'city__name', 'postal_code']
