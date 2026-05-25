abd85d956ded35292526afb6e9a6011458dcbc84
saeed <msabdi1377@gmail.com>
2026-05-26 00:21:02 +0330

    feat: user models

--- user/models.py
+++ user/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from core.models import BaseModel
 
-# Create your models here.
+class User(AbstractUser, BaseModel):
+    username = None
+    phone_number = models.CharField('شماره موبایل', max_length=11, unique=True)
+    email = models.EmailField('ایمیل', null=True, blank=True)
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.phone_number
+
+
+class Province(BaseModel):
+    name = models.CharField('نام استان', max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class City(BaseModel):
+    province = models.ForeignKey(Province, on_delete=models.PROTECT, related_name='cities')
+    name = models.CharField('نام شهر', max_length=50)
+
+    def __str__(self):
+        return f"{self.province.name} - {self.name}"
+
+
+class Address(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='addresses')
+    city = models.ForeignKey(City, on_delete=models.PROTECT, verbose_name='City')
+    address_detail = models.TextField('جزئیات آدرس')
+    postal_code = models.CharField('کد پستی', max_length=10, blank=True)
+
+    def __str__(self):
+        return f"{self.user.phone_number} - {self.city.name}"
