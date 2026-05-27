9d20aa414f1832b8f2f0b37cb2fc07ece3dbde17
saeed <msabdi1377@gmail.com>
2026-05-28 00:44:30 +0330

    refactor: send and verify otp views

--- user/serializers.py
+++ user/serializers.py
@@ -1,10 +1,17 @@
+import random
 import re
+from datetime import datetime, timedelta
+
+from django.core.cache import cache
+from django.conf import settings
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
+import jwt
 
 from core.base_serializers import CustomSerializer
 from user.models import User
 
+from decouple import config
 
 class SendOTPSerializer(CustomSerializer):
     phone_number = serializers.CharField(max_length=11, min_length=11)
@@ -20,13 +27,46 @@ class SendOTPSerializer(CustomSerializer):
             })
         return attrs
 
+    def create(self, validated_data):
+        phone_number = validated_data.get('phone_number')
+
+        otp_code = str(random.randint(1000, 9999))
+
+        cache.set(f"otp_{phone_number}", otp_code, timeout=120)
+
+        # TODO: Implement actual SMS provider sending logic here
+        print(f"\n[SMS LOG] OTP for {phone_number}: {otp_code}\n")
+        return validated_data
+
 
 class VerifyOTPSerializer(CustomSerializer):
     phone_number = serializers.CharField(max_length=11)
-    otp = serializers.CharField(max_length=4, min_length=4)
+    otp = serializers.CharField(max_length=4, min_length=4, write_only=True)
 
     def validate_serializer(self, attrs, error_obj):
+        phone_number = attrs.get('phone_number')
+        otp_received = attrs.get('otp')
+
+        master_otp = config('MASTER_OTP_CODE')
+        cached_otp = cache.get(f"otp_{phone_number}")
+
+        if (master_otp and otp_received == str(master_otp)) or (otp_received == cached_otp):
+            error_obj.append_errors({
+                "message": "کد تایید نادرست است",
+                "reason": "otp"
+            })
         return attrs
+
+    def create(self, validated_data):
+        phone_number = validated_data.get('phone_number')
+        user, created = User.objects.get_or_create(phone_number=phone_number)
+
+        refresh = RefreshToken.for_user(user)
+        validated_data.set('access', str(refresh.access_token))
+        validated_data.set('refresh', str(refresh))
+        validated_data.set('is_new_user', str(created))
+
+        return validated_data
 
 
 class SetPasswordSerializer(CustomSerializer):
--- user/views.py
+++ user/views.py
@@ -1,73 +1,18 @@
-import random
-from django.core.cache import cache
-from django.conf import settings
-from django.contrib.auth import get_user_model
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from core.base_views import CustomCreateAPIView
-from .serializers import SendOTPSerializer, VerifyOTPSerializer, SetPasswordSerializer, LoginSerializer
-from user.models import User
+from .serializers import SendOTPSerializer, VerifyOTPSerializer, SetPasswordSerializer, LoginSerializer, \
+    PasswordResetSendSerializer, PasswordResetVerifySerializer
 
-from decouple import config
 
 
 class SendOTPView(CustomCreateAPIView):
     serializer_class = SendOTPSerializer
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        phone_number = serializer.validated_data['phone_number']
-
-        otp_code = str(random.randint(1000, 9999))
-
-        cache.set(f"otp_{phone_number}", otp_code, timeout=120)
-
-        # TODO: Implement actual SMS provider sending logic here
-        print(f"\n[SMS LOG] OTP for {phone_number}: {otp_code}\n")
-
-        return Response({
-            'message': "OTP code is sent successfully",
-            'data': {"phone_number": phone_number}
-        }, status=status.HTTP_200_OK)
-
 
 class VerifyOTPView(CustomCreateAPIView):
     serializer_class = VerifyOTPSerializer
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        phone_number = serializer.validated_data['phone_number']
-        otp_received = serializer.validated_data['otp']
-
-        master_otp = config('MASTER_OTP_CODE')
-        cached_otp = cache.get(f"otp_{phone_number}")
-
-        if (master_otp and otp_received == str(master_otp)) or (otp_received == cached_otp):
-            user, created = User.objects.get_or_create(phone_number=phone_number)
-
-            refresh = RefreshToken.for_user(user)
-
-            return Response({
-                'message': "SUCCESS",
-                'data': {
-                    'access': str(refresh.access_token),
-                    'refresh': str(refresh),
-                    'is_new_user': created
-                }
-            }, status=status.HTTP_200_OK)
-
-        return Response({
-            'message': "Invalid OTP",
-            'data': None
-        }, status=status.HTTP_400_BAD_REQUEST)
 
 
 class SetPasswordView(CustomCreateAPIView):
