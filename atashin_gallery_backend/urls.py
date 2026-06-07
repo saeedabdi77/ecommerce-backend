bd7b264f18530ae9a8dd92d9ce2a547f74787876
saeed <msabdi1377@gmail.com>
2026-06-07 22:50:13 +0330

    fix: static urls

--- atashin_gallery_backend/urls.py
+++ atashin_gallery_backend/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import permissions
 
@@ -47,4 +49,4 @@ urlpatterns = [
     path(f'api/v{VERSION}/auth/', include('user.auth_urls')),
     path(f'api/v{VERSION}/user/', include('user.urls')),
     path(f'api/v{VERSION}/locations/', include('user.location_urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
