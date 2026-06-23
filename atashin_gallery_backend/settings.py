c25d837ec5d61d24263a3f2fac313de039cc0da5
SaeedAbdi <msabdi1377@gmail.com>
2026-06-23 14:28:00 +0330

    fix: settings

--- atashin_gallery_backend/settings.py
+++ atashin_gallery_backend/settings.py
@@ -237,3 +237,10 @@ SWAGGER_SETTINGS = {
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'http://127.0.0.1:8000',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
