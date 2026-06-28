dd7338e9d45388b5300b407941c303be4e2586da
SaeedAbdi <msabdi1377@gmail.com>
2026-06-28 14:47:06 +0330

    feat: GameRateAdmin

--- installation/admin.py
+++ installation/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import InstallationDeviceType, Game, InstallationRequest
+from .models import InstallationDeviceType, Game, GameRate, InstallationRequest
 
 
 @admin.register(InstallationDeviceType)
@@ -11,13 +11,28 @@ class InstallationDeviceTypeAdmin(admin.ModelAdmin):
     ordering = ("order", "name")
 
 
+class GameRateInline(admin.TabularInline):
+    model = GameRate
+    extra = 1
+    autocomplete_fields = ("game",)
+
+
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
-    list_display = ("name", "device_type", "price", "size", "active", "created_at")
+    list_display = ("name", "price", "size", "active", "created_at")
     list_editable = ("price", "active")
     list_filter = ("device_type", "active")
     search_fields = ("name",)
-    autocomplete_fields = ("device_type",)
+    filter_horizontal = ("device_type",)
+    inlines = (GameRateInline,)
+
+
+@admin.register(GameRate)
+class GameRateAdmin(admin.ModelAdmin):
+    list_display = ("game", "source", "rate", "created_at")
+    list_filter = ("source",)
+    search_fields = ("game__name", "source")
+    autocomplete_fields = ("game",)
 
 
 @admin.register(InstallationRequest)
