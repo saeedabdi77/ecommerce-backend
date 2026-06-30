c8e4a4b1f7acfe96f1ba43ade373f8f76485b8bd
SaeedAbdi <msabdi1377@gmail.com>
2026-06-30 16:09:19 +0330

    refactor: InstallationRequestAdmin

--- installation/admin.py
+++ installation/admin.py
@@ -61,9 +61,10 @@ class InstallationRequestItemInline(admin.TabularInline):
 
 @admin.register(InstallationRequest)
 class InstallationRequestAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "device_type", "total_price", "created_at")
-    list_filter = ("device_type", "created_at")
+    list_display = ("id", "user", "device_type", "status", "total_price", "created_at")
+    list_filter = ("status", "device_type", "created_at")
     search_fields = ("user__phone_number",)
     autocomplete_fields = ("user", "device_type")
     readonly_fields = ("total_price", "created_at", "updated_at")
+    list_editable = ("status",)
     inlines = (InstallationRequestItemInline,)
