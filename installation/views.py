b141a42f98514c43c38cfbde2b73ce63a4a236c8
saeed <msabdi1377@gmail.com>
2026-07-06 17:34:47 +0330

    refactor: add delete method to InstallationRequestItemViewSet

--- installation/views.py
+++ installation/views.py
@@ -2,12 +2,13 @@ from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 from core.base_views import CustomListAPIView, CustomRetrieveAPIView, CustomCreateAPIView, \
     CustomCreateListUpdateDestroyViewSet
 from installation.enums import InstallationRequestStatus
 from installation.filters import GameFilter
-from installation.models import InstallationDeviceType, Game, InstallationRequest
+from installation.models import InstallationDeviceType, Game, InstallationRequest, InstallationRequestItem
 from installation.serializers import InstallationDeviceTypeSerializer, GameSerializer, \
     InstallationRequestRetrieveSerializer, AddInstallationRequestItemSerializer
 from installation.utilities import resolve_draft_installation_request
@@ -53,7 +54,30 @@ class DraftInstallationRequestRetrieveView(CustomRetrieveAPIView):
 
 
 class InstallationRequestItemViewSet(CustomCreateListUpdateDestroyViewSet):
-    http_method_names = ("post",)
+    http_method_names = ("post", "delete")
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                name='guest_uid',
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                format=openapi.FORMAT_UUID,
+                required=False
+            )
+        ]
+    )
+    def delete(self, request, *args, **kwargs):
+        return self.force_destroy(request, *args, **kwargs)
+
+    def get_object(self):
+        guest_uid = self.request.query_params.get('guest_uid')
+        user = self.request.user
+        pk = self.kwargs['pk']
+        draft_installation_request = resolve_draft_installation_request(user, guest_uid)
+        if draft_installation_request:
+            return get_object_or_404(InstallationRequestItem, installation_request=draft_installation_request, id=pk)
+        raise Http404
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
