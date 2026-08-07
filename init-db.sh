a78b1bf43362f4a1179e20ec42ecea413bd849b9
saeed <msabdi1377@gmail.com>
2026-08-07 12:13:41 +0330

    feat: init-db.sh

---
+++ init-db.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
+    CREATE DATABASE ${DATABASE_NAME};
+    CREATE DATABASE ${REPAIR_CONSOLE_DATABASE_NAME};
+EOSQL
