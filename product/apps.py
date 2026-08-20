a615c0aeec7fa1c9a52a8e4fa4f8ad5d574736a5
saeed <msabdi1377@gmail.com>
2026-08-21 00:35:44 +0330

    feat: add base manager

--- product/apps.py
+++ product/apps.py
@@ -5,3 +5,6 @@ class ProductConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'product'
     verbose_name = 'محصول'
+
+    def ready(self):
+        from product import managers
