c679724da439a56a0d1620a0d600f79a66ce2a68
saeed <msabdi1377@gmail.com>
2026-08-28 01:11:00 +0330

    refactor: ProductListView sort

--- product/views.py
+++ product/views.py
@@ -1,10 +1,12 @@
+from django.db.models import Exists, OuterRef
 from rest_framework.permissions import AllowAny
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 
 from core.base_views import CustomListAPIView
+from product.enums import ProductState
 from product.filters import BrandFilter, CategoryFilter, ProductFilter
-from product.models import Brand, Category, ProductType
+from product.models import Brand, Category, Product, ProductType
 from product.serializers import BrandSerializer, CategorySerializer, ProductListSerializer
 
 
@@ -36,3 +38,17 @@ class ProductListView(CustomListAPIView):
     ordering_fields = ('sell_price', 'main_price', 'created_at', 'order')
     ordering = ('-created_at',)
     permission_classes = (AllowAny,)
+
+    def get_queryset(self):
+        return super().get_queryset().annotate(
+            has_stock=Exists(
+                Product.objects.filter(
+                    product_type_id=OuterRef('pk'),
+                    state=ProductState.IN_WAREHOUSE,
+                )
+            ),
+        )
+
+    def filter_queryset(self, queryset):
+        queryset = super().filter_queryset(queryset)
+        return queryset.order_by('-has_stock', *queryset.query.order_by)
