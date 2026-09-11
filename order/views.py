c26e0962e64c1092dde03c72496ac2bb4e78535b
saeed <msabdi1377@gmail.com>
2026-09-11 20:30:24 +0330

    restyle: DeliveryMethodListView

--- order/views.py
+++ order/views.py
@@ -151,7 +151,7 @@ class DeliveryMethodListView(CustomListAPIView):
             raise Http404
 
         if not order.delivery_address:
-            raise ValidationError("ابتدا آدرس ارسال را انتخاب کنید.")
+            raise ValidationError({"message": "ابتدا آدرس ارسال را انتخاب کنید.", "reason": "delivery_address"})
 
         self.order = order
         return get_available_delivery_methods(order)
