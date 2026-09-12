1802b9797ee4a3ef31fe8c2bf858c6d3f3bbf6a3
saeed <msabdi1377@gmail.com>
2026-09-13 00:05:23 +0330

    feat: setup_periodic_tasks

--- config/celery.py
+++ config/celery.py
@@ -1,9 +1,20 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-app = Celery('config')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+celery = Celery("config")
+celery.config_from_object("django.conf:settings", namespace="CELERY")
+celery.autodiscover_tasks()
+
+
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    from order.tasks import expire_submitted_orders
+
+    sender.add_periodic_task(
+        crontab(minute="*/30"),
+        expire_submitted_orders.s(),
+    )
